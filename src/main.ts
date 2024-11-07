2a140ee34a80e4f1f543f3c163e91903d283d251
Artem Smirnov <artem.s13@gmail.com>
2024-11-07 16:46:10 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
@@ -6,6 +7,15 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0.0')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, documentFactory)
 
   const port = process.env.PORT || 4000;
   await app.listen(port);
